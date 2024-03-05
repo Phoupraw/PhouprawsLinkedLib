7fd10db439d2104de625337030b342a9bd5bb916
Phoupraw <467934387@qq.com>
2024-03-05 17:37:39 +0800

    nonEmptyIterator

--- src/main/kotlin/phoupraw/mcmod/linked/fabric/transfer/base/SimpleSingleSlotStorage.kt
+++ src/main/kotlin/phoupraw/mcmod/linked/fabric/transfer/base/SimpleSingleSlotStorage.kt
@@ -1,8 +1,11 @@
 package phoupraw.mcmod.linked.fabric.transfer.base
 
 import net.fabricmc.fabric.api.transfer.v1.storage.base.SingleSlotStorage
+import phoupraw.mcmod.linked.fabric.transfer.storage.empty
+import java.util.*
 
 interface SimpleSingleSlotStorage<T> : SingleSlotStorage<T> {
     override var resource: T
     override var amount: Long
+    override fun nonEmptyIterator(): Iterator<SingleSlotStorage<T>> = if (empty) Collections.emptyIterator() else iterator()
 }
